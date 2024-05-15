30b8b0f8b11e44795309b4daab7afe0a6d3cfd9f
jamie-legg <legg.jamie@gmail.com>
2024-05-15 17:42:14 +0100

    suspense for maze

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Maze from "./components/Maze";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
-   
-   <main>
-   <Maze />
+    <main>
+      <Suspense>
+        <Maze />
+      </Suspense>
     </main>
   );
 }
